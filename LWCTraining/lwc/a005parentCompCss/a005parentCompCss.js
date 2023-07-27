7e6b3619d25330265570b9c70232349bc92f9aad
Mustapha <m.shenkaia@gmail.com>
2023-07-27 14:38:24 +0300

    Combobox Example

--- LWCTraining/lwc/a005parentCompCss/a005parentCompCss.js
+++ LWCTraining/lwc/a005parentCompCss/a005parentCompCss.js
@@ -1,3 +1,37 @@
 import { LightningElement } from 'lwc';
+import displayAcc from '@salesforce/apex/ComboboxExample.getAccounts';
+// getAccounts method olduğu için displayAcc() şeklinde çağrılır. 
 
-export default class A005parentCompCss extends LightningElement {}
+export default class A005parentCompCss extends LightningElement {
+
+    value = ''; 
+    accOptions = []; 
+
+
+    get options() {
+        // return [
+        //     {label: 'Account1', value: 'Value1'},
+        //     {label: 'Account2', value: 'Value2'}
+        // ];
+        return this.accOptions; 
+    }
+
+    connectedCallback(){
+        // promise
+        displayAcc()
+        .then(result =>{
+            // result -> Array 
+            let arr = [];
+            
+            for(let i=0; i<result.length; i++){
+                arr.push({label : result[i].Name, value : result[i].Id});
+            }
+            this.accOptions = arr;
+        })        
+    }
+
+
+    handleonchange(e) {
+        this.value = e.detail.value; 
+    }
+}
